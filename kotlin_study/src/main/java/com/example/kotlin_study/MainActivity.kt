0f3a0c0dbc17b724964654fc9957bcfb23a489c5
GhostLqq <woniu_lqq@163.com>
2022-11-25 09:55:19 +0800

    feat:kotlin练习项目

--- kotlin_study/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ kotlin_study/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -3,25 +3,59 @@ package com.example.kotlin_study
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.IllegalArgumentException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+    var ints = listOf(-1, 2, 3)
+    var map = mapOf("a" to 1, "b" to 2, "c" to 3)
+
+    val p: String by lazy {
+        ""
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("${maxOf(1, 2)}")
 
-        fruits.filter {
-            it.startsWith("a")
-        }.sortedBy {
-            it
-        }.map {
-            it.uppercase()
-        }.forEach {
-            Log.d("liuqianqian", it)
+        val a = 1
+        val b: Long? = a.toLong()
+
+    }
+
+    fun transform(color: String) = when (color) {
+        "red" -> 1
+        "blue" -> 2
+        "green" -> 3
+        else -> throw IllegalArgumentException("Invalid color param value")
+    }
+
+    fun String.spaceToCamelCase() {
+
+    }
+
+    val myObject = object : MyAbstractClass() {
+        override fun doSomething() {
+            TODO("Not yet implemented")
+        }
+
+        override fun sleep() {
+            TODO("Not yet implemented")
         }
     }
+}
+
+abstract class MyAbstractClass {
+    abstract fun doSomething()
+    abstract fun sleep()
+}
+
+class Turle {
+    fun openDown() {}
+    fun penUp() {}
+    fun turn(degrees: Double) {}
+    fun forward(pixels: Double) {}
 }
